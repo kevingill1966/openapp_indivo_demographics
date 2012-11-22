ebc3751a3ec6d7768338d32a8254810594bd1c6d
Kevin Gill <kevin.gill@openapp.ie>
2012-11-22 12:51:41 +0000

    Change suffix on README.txt .rst for github.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='openapp_indivo_demographics',
       version=version,
       description="App to display and edit the Indivo Demographics Record",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
